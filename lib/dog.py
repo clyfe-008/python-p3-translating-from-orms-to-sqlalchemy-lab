6aef8513e61503fc8e0aaf4f4fda9a63b0dbd0a3
Ruan <cliff.kamau@student.moringaschool.com>
2023-09-02 11:05:48 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,38 @@
-from models import Dog
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
-def create_table(base):
-    pass
+Base = declarative_base()
 
-def save(session, dog):
-    pass
+class Dog(Base):
+    __tablename__ = 'dogs'
 
-def get_all(session):
-    pass
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    breed = Column(String)
 
-def find_by_name(session, name):
-    pass
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
+
+def find_by_name(session, name):
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
-def update_breed(session, dog, breed):
-    pass
+def save(session, dog):
+    session.add(dog)
+    session.commit()
+
+def get_all(session):
+    return session.query(Dog).all()
+
+def update_breed(session, dog, new_breed):
+    dog.breed = new_breed
+    session.commit()
